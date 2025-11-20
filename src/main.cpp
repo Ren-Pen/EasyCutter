4acbb35fd3b3a39d99fd99db3c6ece04d51d32af
肖峥瑜 <xzy5487@outlook.com>
2025-11-20 21:40:45 +0900

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include <fmt/core.h>
+#include <wx/wx.h>
 
-int main() {
-    fmt::print("Hello World!\n");
-    return 0;
-}
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit() override
+    {
+        fmt::print("Hello World!\n");
+
+        wxFrame* frame = new wxFrame(
+            nullptr,
+            wxID_ANY,
+            wxT("Title"),
+            wxDefaultPosition,
+            wxSize(250, 150)
+        );
+
+        frame->Centre();
+        frame->Show(true);
+        return true;
+    }
+};
+
+wxIMPLEMENT_APP(MyApp);
 
